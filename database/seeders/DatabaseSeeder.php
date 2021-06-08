54cf297ff01da36d7279a88dfc88cc42f5df797f
ArtemFH <artembereza1@mail.ru>
2021-06-08 22:34:40 +0700

    Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             NominationSeeder::class,
             AwardSeeder::class,
             AdminSeeder::class,
+            UserFakerSeeder::class,
             ModeratorSeeder::class,
             HardwareSeeder::class,
             BenchmarkSeeder::class

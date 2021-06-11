702074cb0aaf30b91d454372bd558c7fed3c456e
ArtemFH <artembereza1@mail.ru>
2021-06-12 01:13:09 +0700

    Commit

---
+++ public/assets/script/media.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', function (e) {
+    if (window.innerWidth >= 992) {
+        var nav = document.getElementById('navbar')
+        var but_end = document.getElementById('but-end');
+        var footer = document.getElementById('footer')
+
+        if (window.scrollY != 0) {
+            nav.classList.add('navbar-scroll')
+            but_end.classList.add('but-end')
+        } else {
+            nav.classList.remove('navbar-scroll')
+            but_end.classList.remove('but-end')
+        }
+
+        if (document.body.clientHeight == window.innerHeight + window.scrollY) {
+            footer.classList.remove('footer-scroll')
+        } else {
+            footer.classList.add('footer-scroll')
+        }
+    }
+});
